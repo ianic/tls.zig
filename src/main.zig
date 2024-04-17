dce8f783474dd80f28724b6dfceafb8282ed8c7f
Igor Anić <igor.anic@gmail.com>
2024-04-17 20:31:46 +0200

    client secret keys calculation

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const fmt = std.fmt;
+const testing = std.testing;
 
 pub fn main() !void {
     const gpa = std.heap.page_allocator;
@@ -38,8 +40,6 @@ const client_hello = [_]u8{
     0x10, 0x04, 0x01, 0x04, 0x03, 0x05, 0x01, 0x05, 0x03, 0x06, 0x01, 0x06, 0x03, 0x02, 0x01, 0x02,
     0x03, 0xff, 0x01, 0x00, 0x01, 0x00, 0x00, 0x12, 0x00, 0x00,
 };
-
-const testing = std.testing;
 
 const server_hello = [_]u8{
     0x16, 0x03, 0x03, 0x00, 0x31, 0x02, 0x00, 0x00, 0x2d, 0x03, 0x03, 0x70, 0x71, 0x72, 0x73, 0x74,
@@ -142,6 +142,40 @@ test "parse server certificate" {
     try testing.expectEqualSlices(u8, &[_]u8{ 0x08, 0x46, 0x36, 0xa0 }, sc.cert[801..]);
 }
 
+const server_key_exchange = [_]u8{
+    0x16, 0x03, 0x03, 0x01, 0x2c, 0x0c, 0x00, 0x01, 0x28, 0x03, 0x00, 0x1d, 0x20, 0x9f, 0xd7, 0xad, 0x6d,
+    0xcf, 0xf4, 0x29, 0x8d, 0xd3, 0xf9, 0x6d, 0x5b, 0x1b, 0x2a, 0xf9, 0x10, 0xa0, 0x53, 0x5b, 0x14, 0x88,
+    0xd7, 0xf8, 0xfa, 0xbb, 0x34, 0x9a, 0x98, 0x28, 0x80, 0xb6, 0x15, 0x04, 0x01, 0x01, 0x00, 0x04, 0x02,
+    0xb6, 0x61, 0xf7, 0xc1, 0x91, 0xee, 0x59, 0xbe, 0x45, 0x37, 0x66, 0x39, 0xbd, 0xc3, 0xd4, 0xbb, 0x81,
+    0xe1, 0x15, 0xca, 0x73, 0xc8, 0x34, 0x8b, 0x52, 0x5b, 0x0d, 0x23, 0x38, 0xaa, 0x14, 0x46, 0x67, 0xed,
+    0x94, 0x31, 0x02, 0x14, 0x12, 0xcd, 0x9b, 0x84, 0x4c, 0xba, 0x29, 0x93, 0x4a, 0xaa, 0xcc, 0xe8, 0x73,
+    0x41, 0x4e, 0xc1, 0x1c, 0xb0, 0x2e, 0x27, 0x2d, 0x0a, 0xd8, 0x1f, 0x76, 0x7d, 0x33, 0x07, 0x67, 0x21,
+    0xf1, 0x3b, 0xf3, 0x60, 0x20, 0xcf, 0x0b, 0x1f, 0xd0, 0xec, 0xb0, 0x78, 0xde, 0x11, 0x28, 0xbe, 0xba,
+    0x09, 0x49, 0xeb, 0xec, 0xe1, 0xa1, 0xf9, 0x6e, 0x20, 0x9d, 0xc3, 0x6e, 0x4f, 0xff, 0xd3, 0x6b, 0x67,
+    0x3a, 0x7d, 0xdc, 0x15, 0x97, 0xad, 0x44, 0x08, 0xe4, 0x85, 0xc4, 0xad, 0xb2, 0xc8, 0x73, 0x84, 0x12,
+    0x49, 0x37, 0x25, 0x23, 0x80, 0x9e, 0x43, 0x12, 0xd0, 0xc7, 0xb3, 0x52, 0x2e, 0xf9, 0x83, 0xca, 0xc1,
+    0xe0, 0x39, 0x35, 0xff, 0x13, 0xa8, 0xe9, 0x6b, 0xa6, 0x81, 0xa6, 0x2e, 0x40, 0xd3, 0xe7, 0x0a, 0x7f,
+    0xf3, 0x58, 0x66, 0xd3, 0xd9, 0x99, 0x3f, 0x9e, 0x26, 0xa6, 0x34, 0xc8, 0x1b, 0x4e, 0x71, 0x38, 0x0f,
+    0xcd, 0xd6, 0xf4, 0xe8, 0x35, 0xf7, 0x5a, 0x64, 0x09, 0xc7, 0xdc, 0x2c, 0x07, 0x41, 0x0e, 0x6f, 0x87,
+    0x85, 0x8c, 0x7b, 0x94, 0xc0, 0x1c, 0x2e, 0x32, 0xf2, 0x91, 0x76, 0x9e, 0xac, 0xca, 0x71, 0x64, 0x3b,
+    0x8b, 0x98, 0xa9, 0x63, 0xdf, 0x0a, 0x32, 0x9b, 0xea, 0x4e, 0xd6, 0x39, 0x7e, 0x8c, 0xd0, 0x1a, 0x11,
+    0x0a, 0xb3, 0x61, 0xac, 0x5b, 0xad, 0x1c, 0xcd, 0x84, 0x0a, 0x6c, 0x8a, 0x6e, 0xaa, 0x00, 0x1a, 0x9d,
+    0x7d, 0x87, 0xdc, 0x33, 0x18, 0x64, 0x35, 0x71, 0x22, 0x6c, 0x4d, 0xd2, 0xc2, 0xac, 0x41, 0xfb,
+};
+
+test "parse server key exchange" {
+    var stream = std.io.fixedBufferStream(&server_key_exchange);
+    const reader = stream.reader();
+
+    const rh = try RecordHeader.parse(reader);
+    try testing.expectEqual(RecordHeader.Kind.handshake, rh.kind);
+    try testing.expectEqual(300, rh.size);
+
+    const hh = try HandshakeHeader.parse(reader);
+    try testing.expectEqual(HandshakeHeader.Kind.server_key_exchange, hh.kind);
+    try testing.expectEqual(296, hh.size);
+}
+
 const RecordHeader = struct {
     const Kind = enum(u8) {
         alert = 0x15,
@@ -179,13 +213,14 @@ const HandshakeHeader = struct {
     const Kind = enum(u8) {
         server_hello = 0x02,
         server_certificate = 0x0b,
+        server_key_exchange = 0x0c,
     };
     kind: Kind,
     size: u24,
 
     fn parse(reader: anytype) !HandshakeHeader {
         const k = try reader.readByte();
-        if (!(k == 0x02 or k == 0x0b)) return error.UnknownHandshakeHeaderKind;
+        if (!(k == 0x02 or k == 0x0b or k == 0x0c)) return error.UnknownHandshakeHeaderKind;
         const size = try reader.readInt(u24, .big);
         return .{
             .kind = @enumFromInt(k),
@@ -252,3 +287,122 @@ const ServerCertificate = struct {
 //     const e: RecordHeader.Kind = @enumFromInt(0x14);
 //     std.debug.print("e: {}\n", .{e});
 // }
+
+test "client key exchange generation" {
+    const sha256 = std.crypto.auth.hmac.sha2.HmacSha256.create;
+    const bytesToHex = std.fmt.bytesToHex;
+
+    var client_private_key: [32]u8 = undefined;
+    var server_random: [32]u8 = undefined;
+    var client_random: [32]u8 = undefined;
+    var server_public_key: [32]u8 = undefined;
+    _ = try fmt.hexToBytes(client_private_key[0..], "202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f");
+    _ = try fmt.hexToBytes(server_random[0..], "707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f");
+    _ = try fmt.hexToBytes(client_random[0..], "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f");
+    _ = try fmt.hexToBytes(server_public_key[0..], "9fd7ad6dcff4298dd3f96d5b1b2af910a0535b1488d7f8fabb349a982880b615");
+
+    // public from private key
+    {
+        const client_public_key = try std.crypto.dh.X25519.recoverPublicKey(client_private_key);
+        try std.testing.expectEqualStrings(
+            "358072d6365880d1aeea329adf9121383851ed21a28e3b75e965d0d2cd166254",
+            &bytesToHex(client_public_key, .lower),
+        );
+    }
+
+    var master_secret: [32 + 16]u8 = undefined;
+    {
+        // pre master secret calculation
+        const pre_master_secret = try std.crypto.dh.X25519.scalarmult(client_private_key, server_public_key);
+        try std.testing.expectEqualStrings(
+            "df4a291baa1eb7cfa6934b29b474baad2697e29f1f920dcc77c8a0a088447624",
+            &bytesToHex(pre_master_secret, .lower),
+        );
+
+        // master secret calculation
+        const prefix = "master secret";
+        var a_seed: [prefix.len + 32 * 2]u8 = undefined;
+        @memcpy(a_seed[0..prefix.len], prefix);
+        @memcpy(a_seed[prefix.len..][0..32], &client_random);
+        @memcpy(a_seed[prefix.len + 32 ..], &server_random);
+
+        var a1: [32]u8 = undefined;
+        var a2: [32]u8 = undefined;
+        sha256(&a1, &a_seed, &pre_master_secret);
+        sha256(&a2, &a1, &pre_master_secret);
+
+        var p_seed: [a_seed.len + 32]u8 = undefined;
+        @memcpy(p_seed[0..a1.len], &a1);
+        @memcpy(p_seed[a1.len..], &a_seed);
+
+        var p1: [32]u8 = undefined;
+        var p2: [32]u8 = undefined;
+        sha256(&p1, &p_seed, &pre_master_secret);
+
+        @memcpy(p_seed[0..a1.len], &a2);
+        @memcpy(p_seed[a1.len..], &a_seed);
+        sha256(&p2, &p_seed, &pre_master_secret);
+
+        @memcpy(master_secret[0..32], &p1);
+        @memcpy(master_secret[32..], p2[0..16]);
+
+        try testing.expectEqualStrings(
+            "916abf9da55973e13614ae0a3f5d3f37b023ba129aee02cc9134338127cd7049781c8e19fc1eb2a7387ac06ae237344c",
+            &bytesToHex(master_secret, .lower),
+        );
+    }
+
+    { // final encryption keys
+        const prefix = "key expansion";
+        var a_seed: [prefix.len + 32 * 2]u8 = undefined;
+        @memcpy(a_seed[0..prefix.len], prefix);
+        @memcpy(a_seed[prefix.len..][0..32], &server_random);
+        @memcpy(a_seed[prefix.len + 32 ..], &client_random);
+
+        const a0 = a_seed;
+        var a1: [32]u8 = undefined;
+        var a2: [32]u8 = undefined;
+        var a3: [32]u8 = undefined;
+        var a4: [32]u8 = undefined;
+        sha256(&a1, &a0, &master_secret);
+        sha256(&a2, &a1, &master_secret);
+        sha256(&a3, &a2, &master_secret);
+        sha256(&a4, &a3, &master_secret);
+
+        var p1: [32]u8 = undefined;
+        var p2: [32]u8 = undefined;
+        var p3: [32]u8 = undefined;
+        var p4: [32]u8 = undefined;
+
+        var p_seed: [a_seed.len + 32]u8 = undefined;
+        @memcpy(p_seed[32..], &a_seed);
+        @memcpy(p_seed[0..32], &a1);
+        sha256(&p1, &p_seed, &master_secret);
+        @memcpy(p_seed[0..32], &a2);
+        sha256(&p2, &p_seed, &master_secret);
+        @memcpy(p_seed[0..32], &a3);
+        sha256(&p3, &p_seed, &master_secret);
+        @memcpy(p_seed[0..32], &a4);
+        sha256(&p4, &p_seed, &master_secret);
+
+        var p: [32 * 4]u8 = undefined;
+        @memcpy(p[0..32], &p1);
+        @memcpy(p[32..64], &p2);
+        @memcpy(p[64..96], &p3);
+        @memcpy(p[96..], &p4);
+
+        const client_write_mac_key = p[0..20];
+        const server_write_mac_key = p[20..40];
+        const client_write_key = p[40..56];
+        const server_write_key = p[56..72];
+        const client_write_iv = p[72..88];
+        const server_write_iv = p[88..104];
+
+        try testing.expectEqualStrings("1b7d117c7d5f690bc263cae8ef60af0f1878acc2", &bytesToHex(client_write_mac_key, .lower));
+        try testing.expectEqualStrings("2ad8bdd8c601a617126f63540eb20906f781fad2", &bytesToHex(server_write_mac_key, .lower));
+        try testing.expectEqualStrings("f656d037b173ef3e11169f27231a84b6", &bytesToHex(client_write_key, .lower));
+        try testing.expectEqualStrings("752a18e7a9fcb7cbcdd8f98dd8f769eb", &bytesToHex(server_write_key, .lower));
+        try testing.expectEqualStrings("a0d2550c9238eebfef5c32251abb67d6", &bytesToHex(client_write_iv, .lower));
+        try testing.expectEqualStrings("434528db4937d540d393135e06a11bb8", &bytesToHex(server_write_iv, .lower));
+    }
+}
